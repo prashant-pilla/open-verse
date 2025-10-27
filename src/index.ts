3894dd60f4d380f69d8ef82c3112ef53aa1be5d4
prashant-pilla <pilla146@umn.edu>
2025-10-26 22:09:12 -0400

    fix(ts-node): remove .ts extension in orchestrator import to satisfy TS5097

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import pino from 'pino';
-import { Orchestrator } from './orchestrator.ts';
+import { Orchestrator } from './orchestrator';
 import { loadConfig } from './config';
 import { loadModelsFromEnv } from './modelLoader';
 
