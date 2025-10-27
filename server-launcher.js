fd6b1b638ab713320e89cce33eb4bc8091de3b1c
prashant-pilla <pilla146@umn.edu>
2025-10-26 20:24:16 -0400

    Use ts-node register API instead of CLI to fix path resolution

--- server-launcher.js
+++ server-launcher.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 // Simple launcher that ensures correct working directory
 const path = require('path');
-const { execSync } = require('child_process');
 
 // Change to script directory (repo root)
 process.chdir(__dirname);
 
-// Run ts-node with the server file
-execSync('npx ts-node src/server.ts', { stdio: 'inherit' });
+// Register ts-node and run the server
+require('ts-node').register({
+  transpileOnly: true,
+  compilerOptions: {
+    module: 'commonjs'
+  }
+});
+
+// Now require the TypeScript server file
+require('./src/server.ts');
 
