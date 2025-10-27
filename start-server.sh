ff9fbc7b937baad36c55e574274de355bb1814d6
prashant-pilla <pilla146@umn.edu>
2025-10-26 20:08:00 -0400

    Use absolute path in start script

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-exec npx ts-node src/server.ts
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+exec node -r ts-node/register "$SCRIPT_DIR/src/server.ts"
 
