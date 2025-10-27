47deadb5df667527d4338bd4cecc346f20b6d0b5
prashant-pilla <pilla146@umn.edu>
2025-10-26 20:34:14 -0400

    Add debug logging to diagnose path issue

--- server-launcher.js
+++ server-launcher.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 // Simple launcher that ensures correct working directory
 const path = require('path');
+const fs = require('fs');
 
 // Change to script directory (repo root)
 process.chdir(__dirname);
+
+// Debug: Check if src/server.ts exists
+const serverPath = path.join(__dirname, 'src', 'server.ts');
+console.log('Looking for server at:', serverPath);
+console.log('File exists:', fs.existsSync(serverPath));
+console.log('__dirname:', __dirname);
+console.log('Contents of __dirname:', fs.readdirSync(__dirname).slice(0, 10));
 
 // Register ts-node and run the server
 require('ts-node').register({
@@ -14,5 +22,5 @@ require('ts-node').register({
 });
 
 // Now require the TypeScript server file using absolute path
-require(path.join(__dirname, 'src', 'server.ts'));
+require(serverPath);
 
