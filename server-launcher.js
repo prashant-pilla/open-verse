d31ad491e3aea7d49e8a9f1f428d3155a38fe7bb
prashant-pilla <pilla146@umn.edu>
2025-10-26 20:36:53 -0400

    Show full directory listing to diagnose missing src folder

--- server-launcher.js
+++ server-launcher.js
@@ -11,7 +11,11 @@ const serverPath = path.join(__dirname, 'src', 'server.ts');
 console.log('Looking for server at:', serverPath);
 console.log('File exists:', fs.existsSync(serverPath));
 console.log('__dirname:', __dirname);
-console.log('Contents of __dirname:', fs.readdirSync(__dirname).slice(0, 10));
+console.log('Full contents of __dirname:', fs.readdirSync(__dirname));
+console.log('Does src dir exist?', fs.existsSync(path.join(__dirname, 'src')));
+if (fs.existsSync(path.join(__dirname, 'src'))) {
+  console.log('Contents of src dir:', fs.readdirSync(path.join(__dirname, 'src')).slice(0, 10));
+}
 
 // Register ts-node and run the server
 require('ts-node').register({
